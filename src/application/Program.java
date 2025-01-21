26bf928c411b269f8690b36f5491d6fab6133e1b
Claudio Filho <claudiocsfilho@gmail.com>
2025-01-21 15:59:32 -0300

    Implementando o método findAll da classe SellerDaoJDBC e realizando teste 3 na classe Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,9 +16,15 @@ public class Program {
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
-        System.out.println("=== TEST 2: seller findByDepartment ===");
+        System.out.println("\n=== TEST 2: seller findByDepartment ===");
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller s : list){
+            System.out.println(s);
+        }
+
+        System.out.println("\n=== TEST 3: seller findAll ===");
+        list = sellerDao.findAll();
         for (Seller s : list){
             System.out.println(s);
         }
