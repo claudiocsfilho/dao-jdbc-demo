afd02122cee98f232152b2e88e57466a29d1059f
Claudio Filho <claudiocsfilho@gmail.com>
2025-01-21 17:46:58 -0300

    implementando o método delete() na classe SellerDaoJDBC e aplicando teste 6 em Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,9 +7,12 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -18,7 +21,7 @@ public class Program {
         System.out.println(seller);
 
         System.out.println("\n=== TEST 2: seller findByDepartment ===");
-        Department department = new Department(2, null);
+          Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
         for (Seller s : list){
             System.out.println(s);
@@ -41,5 +44,12 @@ public class Program {
         sellerDao.update(seller);
         System.out.println("Update completed!");
 
+        System.out.println("\n=== TEST 6: seller delete ===");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed!");
+
+        sc.close();
     }
 }
