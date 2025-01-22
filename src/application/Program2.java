1d28cc7f8fffc2530b5003a9fca0fdc8e0248bc1
Claudio Filho <claudiocsfilho@gmail.com>
2025-01-22 17:11:25 -0300

    Implementando métodos update() e findById() na classe 'DepartmentDaoJDBC', aplicando testes 2 e 3 em Program2

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -2,6 +2,7 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
+import model.dao.SellerDao;
 import model.entities.Department;
 
 import java.util.Scanner;
@@ -13,10 +14,21 @@ public class Program2 {
 
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
-        System.out.println("=== TEST 1: Department insert ===");
-        Department department1 = new Department(null, "Arts");
-        departmentDao.insert(department1);
-        System.out.println("Inserted! New id: " + department1.getId());
+//        System.out.println("=== TEST 1: Department insert ===");
+//        Department department1 = new Department(null, "Arts");
+//        departmentDao.insert(department1);
+//        System.out.println("Inserted! New id: " + department1.getId());
+
+        System.out.println("\n=== TEST 2: Department findById ===");
+        Department department2 = departmentDao.findById(2);
+        System.out.println(department2);
+
+        System.out.println("\n=== TEST 3: Department update ===");
+        department2 = departmentDao.findById(2);
+        department2.setName("ElectronicsUpdated");
+        departmentDao.update(department2);
+        System.out.println("Updated!");
+
 
 
 
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,6 +4,7 @@ import db.DB;
 import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.*;
 import java.util.List;
@@ -49,7 +50,22 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void update(Department obj) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement(
+                    "UPDATE department SET Name = ? WHERE Id = ?");
 
+            st.setString(1, obj.getName());
+            st.setInt(2, obj.getId());
+
+            st.executeUpdate();
+        }
+        catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
@@ -59,11 +75,54 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public Department findById(Integer id) {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                "SELECT department.*, seller.Id as SellerId, seller.Name as SellerName, seller.Email, seller.BirthDate, seller.BaseSalary "
+                    + "FROM department INNER JOIN seller "
+                    + "ON department.Id = seller.DepartmentId "
+                    + "WHERE department.Id = ?");
+            st.setInt(1, id);
+            rs = st.executeQuery();
+
+            if (rs.next()){
+                Department dep = instantiateDepartment(rs);
+                return dep;
+            }
+            return null;
+        }
+        catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
     public List<Department> findAll() {
         return List.of();
     }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("Id"));
+        dep.setName(rs.getString("Name"));
+        return dep;
+    }
+
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+        Seller sel = new Seller();
+        sel.setId(rs.getInt("SellerId"));
+        sel.setName(rs.getString("SellerName"));
+        sel.setEmail(rs.getString("Email"));
+        sel.setBirthDate(rs.getDate("BirthDate"));
+        sel.setBaseSalary(rs.getDouble("BaseSalary"));
+        sel.setDepartment(dep);
+        return sel;
+    }
+
 }
+
